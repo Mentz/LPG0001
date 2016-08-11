a1246627b83d338ba03b73c4ec7d05449291d8e7
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2016-08-11 18:44:37 -0300

    Adding files as of 18:44:37 08/11/16

--- 20160811_for_exemplo.c
+++ 20160811_for_exemplo.c
@@ -2,13 +2,13 @@
 #include <math.h>
 
 int main(){
-	double t = 0.05; // percentual de rendimento ao ano de poupança (forma decimal)
+	double t = 0.05; // rendimento ao ano de poupança ao ano
 	int n = 10; // anos para cálculo do rendimento
 	double vp = 1000; // quantidade de investimento inicial em reais
 	double vf = 0; // valor final no término de cada ano
 	int ano;
 	printf("%-10s%10s\n","Ano", "Saldo");
-	for (ano = 0; ano <= 10; ano++) {
+	for (ano = 0; ano <= n; ano++) {
 		vf = vp*pow(1+t,ano);
 		printf("%-10d%10.2f\n",ano,vf);
 	}

bef110270b6024314d0a076ea9c2cdb2654d0de0
Mentz <Mentz@users.noreply.github.com>
2016-09-01 18:40:17 -0300

    Create programa.c

---
+++ 20160901/programa.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "lpg.h"
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+	int notas[ALUNOS][PROVAS] = {0};
+	int i, j;
+	int op;
+	srand(time(NULL));
+	for (i = 0; i < ALUNOS; i++) {
+		for (j = 0; j < PROVAS; j++) {
+			notas[i][j] = rand()%10;
+		}
+	}
+
+	while ((op = menu()) != 0) {
+		if (op==1) {
+			imprimir(notas);
+		} else if (op==2) {
+			printf("Média da turma = %.2f\n",media_turma(nota));
+		} else if (op==3) {
+			printf("Maior nota = %.2f\n",maior_nota(notas));
+		} else if (op==4) {
+			printf("Menor nota = %.2f\n",menor_nota(notas));
+		} else if (op==5) {
+			printf("Média dos alunos\n");
+			for (i = 0; i < ALUNOS; i++) {
+				printf("Alunos %d = %.2f\n",i,media_aluno(notas[i]));
+			}
+		} else {
+			printf("Opção inválida\n");
+		}
+	} //fim while
+
+	return 0;
+}

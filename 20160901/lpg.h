5c55c8cdd3eeeb6c73f63f900c5389b11c548d2d
Mentz <Mentz@users.noreply.github.com>
2016-09-01 18:40:35 -0300

    Create lpg.h

---
+++ 20160901/lpg.h
@@ -0,0 +1,12 @@
+#ifndef LPG_H
+#define LPG_H
+#define ALUNOS 10
+#define PROVAS 5
+
+float media_aluno(int notas[]);
+float media_turma(int notas[][PROVAS]);
+void imprimir(int notas[][PROVAS]);
+int maior_nota(int notas[][PROVAS]);
+int menor_nota(int notas[][PROVAS]);
+
+#endif
